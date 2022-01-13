d4894bacb7c87c72cb741f99ff403b1c61aef5af
Asp Prothes <75435992+Prothes-asp@users.noreply.github.com>
2022-01-13 21:11:43 +0600

    Add files via upload

---
+++ Gost Python/Gost.py
@@ -0,0 +1,15 @@
+import pygame
+from time import sleep
+pygame.init()
+window=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+pygame.mixer.init()
+pygame.mixer.music.load("horror.mp3")
+pygame.mixer.music.play()
+sleep(2)
+pygame.mixer.music.load("bhoot.mp3")
+pygame.mixer.music.play()
+sleep(1)
+image=pygame.image.load("gst.jpg")
+window.blit(image,(0,0))
+pygame.display.update()
+sleep(1)
